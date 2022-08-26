6eca977fbe46e41a1c5c14c520597cefda4c6dd9
lahooper <lukehooper2@gmail.com>
2022-08-26 12:07:34 +0100

    End section 8 - react routing

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,19 +1,18 @@
 import axios, { AxiosResponse } from 'axios';
-import { isImportEqualsDeclaration } from 'typescript';
 import { Activity } from '../models/activity';
 
 const sleep = (delay: number) => {
-    return new Promise((resolve)=>{
+    return new Promise((resolve) => {
         setTimeout(resolve, delay)
     })
 }
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
-axios.interceptors.response.use(async response =>{
-    try{
+axios.interceptors.response.use(async response => {
+    try {
         await sleep(1000)
         return response;
-    } catch (e){
+    } catch (e) {
 
         console.log(e);
         return Promise.reject(e);
@@ -21,19 +20,19 @@ axios.interceptors.response.use(async response =>{
 })
 
 
-const responseBody = <T> ( response: AxiosResponse<T>) => response.data;
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
-const request={
-    get: <T> (url: string) => axios.get<T>(url).then(responseBody),
-    post: <T> (url: string, body: {}) => axios.post<T> (url, body).then(responseBody),
-    put: <T> (url: string, body: {}) => axios.put<T> (url, body).then(responseBody),
-    del: <T> (url: string) => axios.delete<T> (url).then(responseBody),
+const request = {
+    get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+    post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+    del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 }
 
 const Activities = {
     list: () => request.get<Activity[]>('/activities'),
     details: (id: string) => request.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => request.post<void>('activities', activity) ,
+    create: (activity: Activity) => request.post<void>('activities', activity),
     update: (activity: Activity) => request.put<void>(`/activities/${activity.id}`, activity),
     delete: (id: string) => request.del<void>(`/activities/${id}`)
 }
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,66 +1,85 @@
 import { makeAutoObservable, runInAction } from "mobx"
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
-import {v4 as uuid } from "uuid"
-import { arrayBuffer } from "stream/consumers";
 
-export default class ActivityStore{
+
+
+export default class ActivityStore {
     activityRegistry = new Map<string, Activity>();
     selectedActivity: Activity | undefined = undefined;
-    editMode= false;
-    loading= false;
+    editMode = false;
+    loading = false;
     loadingInitial = true;
 
-    constructor(){
+    constructor() {
         makeAutoObservable(this)
     }
 
-    get activitiesByDate(){
-        return Array.from(this.activityRegistry.values()).sort((a,b)=> 
-        Date.parse(a.date) - Date.parse(b.date));
+    get activitiesByDate() {
+        return Array.from(this.activityRegistry.values()).sort((a, b) =>
+            Date.parse(a.date) - Date.parse(b.date));
     }
 
     loadActivities = async () => {
-        try{
+        this.loadingInitial = true;
+        try {
             const activities = await agent.Activities.list();
-                activities.forEach((activity) => {
-                    activity.date = activity.date.split("T")[0];
-                    this.activityRegistry.set(activity.id, activity)
-                  })
-                  this.setLoadingInitial(false);  
-        } catch (error){
+            activities.forEach((activity) => {
+                this.setActivity(activity);
+            })
+            this.setLoadingInitial(false);
+        } catch (error) {
             console.log(error);
-                this.setLoadingInitial(false)   
+            this.setLoadingInitial(false)
         }
     }
 
-    setLoadingInitial = (state: boolean) =>{
+    loadActivity = async (id: string) => {
+
+        let activity = this.getActivity(id);
+        if (activity) {
+            this.selectedActivity = activity;
+            return activity;
+        } else {
+            this.loadingInitial = true;
+            try {
+                activity = await agent.Activities.details(id);
+                this.setActivity(activity);
+                this.setSelectedActivity(activity);
+                this.setLoadingInitial(false);
+                return activity;
+            } catch (error) {
+                console.log(error);
+                this.setLoadingInitial(false);
+            }
+        }
+    }
+
+    private setActivity = (activity: Activity) => {
+        activity.date = activity.date.split("T")[0];
+        this.activityRegistry.set(activity.id, activity)
+    }
+
+    private getActivity = (id: string) => {
+        return this.activityRegistry.get(id);
+    }
+
+    private setSelectedActivity = (activity: Activity) => {
+        this.selectedActivity = activity;
+    }
+
+    setLoadingInitial = (state: boolean) => {
         this.loadingInitial = state;
-    }  
-
-    selectActivity = (id: string) => {
-        this.selectedActivity = this.activityRegistry.get(id);
     }
 
-    cancelSelectedActivity = () =>{
-        this.selectedActivity = undefined;
-    }
 
-    openForm = (id?: string) =>{
-        id ? this.selectActivity(id) : this.cancelSelectedActivity();
-        this.editMode = true;
-    }
 
-    closeForm = () =>{
-        this.editMode = false;
-    }
 
-    createActivity = async( activity: Activity) =>{
+    createActivity = async (activity: Activity) => {
         this.loading = true;
-        activity.id = uuid();
         try {
             await agent.Activities.create(activity);
-            runInAction(()=>{
+            runInAction(() => {
                 this.activityRegistry.set(activity.id, activity)
                 this.selectedActivity = activity;
                 this.editMode = false;
@@ -68,47 +87,64 @@ export default class ActivityStore{
             })
         } catch (error) {
             console.log(error)
-            runInAction(()=>{
+            runInAction(() => {
                 this.loading = false;
             })
-            
+
         }
     }
 
-    updateActivity = async (activity: Activity) =>{
+    updateActivity = async (activity: Activity) => {
         this.loading = true;
         try {
             await agent.Activities.update(activity);
-            runInAction(()=>{
+            runInAction(() => {
                 this.activityRegistry.set(activity.id, activity)
                 this.selectedActivity = activity;
                 this.editMode = false;
-                this.loading   = false;
+                this.loading = false;
             })
-            
+
         } catch (error) {
             console.log(error)
-            runInAction(()=>{
+            runInAction(() => {
                 this.loading = false;
             })
         }
     }
 
-    deleteActivity = async (id: string) =>{
+    deleteActivity = async (id: string) => {
         this.loading = true;
         try {
             await agent.Activities.delete(id);
-            runInAction(()=>{
+            runInAction(() => {
                 this.activityRegistry.delete(id)
-                if (this.selectedActivity?.id === id ) this.cancelSelectedActivity();
-                this.loading   = false;
+                this.loading = false;
             })
-            
+
         } catch (error) {
             console.log(error)
-            runInAction(()=>{
+            runInAction(() => {
                 this.loading = false;
             })
         }
     }
 }
+
+/*   selectActivity = (id: string) => {
+        this.selectedActivity = this.activityRegistry.get(id);
+    }
+
+    cancelSelectedActivity = () => {
+        this.selectedActivity = undefined;
+    }
+
+    openForm = (id?: string) => {
+        id ? this.selectActivity(id) : this.cancelSelectedActivity();
+        this.editMode = true;
+    }
+
+    closeForm = () => {
+        this.editMode = false;
+    }
+    */
